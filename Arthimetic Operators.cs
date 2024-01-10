d229a1361a13f9db7b52ff8623874df8a13913ee
Umakanthreddy1 <155665119+Umakanthreddy1@users.noreply.github.com>
2024-01-10 13:48:42 -0600

    Add files via upload

---
+++ Arthimetic Operators.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5; int b=2;
+            Console.WriteLine(" Sum : {0}",a+b);
+            Console.WriteLine(" Sub : {0}", a - b);
+            Console.WriteLine(" Mul : {0}", a * b);
+            Console.WriteLine(" Quo : {0}", a / b);
+            Console.WriteLine(" Div : {0}", a % b);
+        }
+    }
+}
