311b4f1dada3f9bec465219c66068f26862d48bd
Umakanthreddy1 <155665119+Umakanthreddy1@users.noreply.github.com>
2024-01-10 11:34:09 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        
+        double sum = number1 + number2;
+
+        
+        Console.WriteLine($"The sum of {number1} and {number2} is: {sum}");
+        
+    }
+}
