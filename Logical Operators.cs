a088bccf9576116625a0ee232267fafeb654ba27
Umakanthreddy1 <155665119+Umakanthreddy1@users.noreply.github.com>
2024-01-11 08:53:27 -0600

    Add files via upload

---
+++ Logical Operators.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; int b = 20;
+            Console.WriteLine(a>b && a<b);
+            Console.WriteLine(a>b == a<b);
+            Console.WriteLine(a>b || a<b);
+            Console.WriteLine(a>b != a<b);
+            bool c=true;
+            Console.WriteLine(!c);
+
+        }
+    }
+}
---
+++ Write Commands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_operators.Properties
+{
+    internal class Write
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("I am Umakanth Reddy");
+            Console.Write("I have 10 years of IT Experience");
+            Console.WriteLine();
+            Console.WriteLine("as a Microsoft .NET Developer");
+        }
+        
+    }
+}
