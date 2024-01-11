b613ad752cc5b29ed8a3d61772bbd478d45554ef
Umakanthreddy1 <155665119+Umakanthreddy1@users.noreply.github.com>
2024-01-11 08:53:53 -0600

    Add files via upload

---
+++ Assignment Operators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2; int b=3;
+            Console.WriteLine("a={0}",a);
+            a += b;
+            Console.WriteLine("a={0}", a);
+            a -= b;
+            Console.WriteLine("a={0}", a);
+            a *= b;
+            Console.WriteLine("a={0}", a);
+            a /= b;
+            Console.WriteLine("a={0}", a);
+
+        }
+    }
+}
